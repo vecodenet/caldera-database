2b74e073eb00e0fdae79062be5ed33df789453b9
biohzrdmx <biohazardmx@gmail.com>
2022-11-02 18:46:34 -0600

    Removed version from docblock

--- src/Database/Adapter/SQLiteAdapter.php
+++ src/Database/Adapter/SQLiteAdapter.php
@@ -5,7 +5,6 @@ declare(strict_types = 1);
 /**
  * Caldera Database
  * Database abstraction layer, part of Vecode Caldera
- * @version 1.0
  * @author  biohzrdmx <github.com/biohzrdmx>
  * @copyright Copyright (c) 2022 Vecode. All rights reserved
  */
